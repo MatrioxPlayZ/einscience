5107fd5b7067a7652aa5a346f3dd8304c69855de
MatrioxPlayZ <shaharyar321321@gmail.com>
2023-09-24 10:43:46 +0500

    initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import HeroComponent from "./components/HeroComponent.tsx";
 import './components/components.css'
+import Details from "./components/Details.tsx";
 
 function App() {
-    
+
     return (
         <>
-
             <HeroComponent/>
+            <Details/>
         </>
     )
 }
---
+++ src/components/Details.tsx
@@ -0,0 +1,30 @@
+import IconDown from "./icons/DownArrow.tsx";
+
+const Details = () => {
+
+
+    return <>
+        <div className="styleBlock"><h1 className={'detailsButton underline-animation'}>EVENT DETAILS</h1>
+            <div className="down" onClick={() => {
+                const contactsSection = document.querySelector('#contacts');
+                if (contactsSection) {
+                    contactsSection.scrollIntoView({behavior: 'smooth'});
+                }
+            }}><IconDown/></div>
+        </div>
+        <div className="eventDetails">
+            <div className="contactUs"><h1>Contact Us</h1>
+
+                <div className="subContact" id={'contacts'}>
+                    <h3>Hassan Shahid: 0321-8890981</h3>
+                    <h3>Hammad Hussain: 0320-7819831</h3>
+                    <h3>Umar Raza: 0323-4468228</h3>
+                </div>
+            </div>
+            <h3 className={'location'}>Pak-Turk Maarif International Schools and Colleges Lahore Boys Campus, Raiwind
+                Road, Block D OPF Housing
+                Scheme, Lahore, Punjab, Pakistan, 54000</h3>
+        </div>
+    </>
+}
+export default Details
