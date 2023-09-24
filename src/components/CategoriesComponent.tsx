46ff2d2b521a0daf18a20a6d97ccecd765956175
MatrioxPlayZ <shaharyar321321@gmail.com>
2023-09-24 12:02:27 +0500

    details

--- src/components/CategoriesComponent.tsx
+++ src/components/CategoriesComponent.tsx
@@ -13,12 +13,15 @@ const CategoriesComponent = () => {
 
 
     const handleHover = (e: any) => {
-        gsap.to(e.currentTarget, {duration: 0.1, opacity: 0.5, ease: 'ease-out', y: -20})
-        // gsap.to(e.target.children[1], {duration: 0.1, opacity: 0.5, ease: 'ease-out', y: -20})
+        gsap.to(e.currentTarget.children[1], {duration: 0.1, opacity: 0.01, ease: 'ease-out'})
+        gsap.to(e.currentTarget.children[0], {duration: 0.1, opacity: 0.01, ease: 'ease-out'})
+        gsap.to(e.currentTarget.children[2], {duration: 0.1, opacity: 1, ease: 'ease-out'})
+
     }
     const handleLeave = (e: any) => {
-        gsap.to(e.currentTarget, {duration: 0.1, opacity: 1, ease: 'ease-out', y: 0, delay: 0.1})
-        // gsap.to(e.target.children[1], {duration: 0.1, opacity: 1, ease: 'ease-out', y: 0})
+        gsap.to(e.currentTarget.children, {duration: 0.1, opacity: 1, ease: 'ease-out', y: 0})
+        gsap.to(e.currentTarget.children[2], {duration: 0.1, opacity: 0, ease: 'ease-out'})
+
     }
     return (
         <>
@@ -28,35 +31,59 @@ const CategoriesComponent = () => {
                     <div className={'icon'} id={'robotics'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconRobot/>
                         <div className={'labelText'}>Robotics</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <div className={'icon'} id={'math'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconBxMath/>
                         <div className={'labelText'}>Math Olympiad</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <div className={'icon'} id={'engineer'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <Icon150Cogs/>
                         <div className={'labelText'}>Engineering <br/>Encounters</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <div className={'icon'} id={'syrup'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconMasksTheater/>
                         <div className={'labelText'}>Syrup</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <br/>
                     <div className={'icon'} id={'cerca'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconHeadLightbulb/>
                         <div className={'labelText'}>Cerca-Trova</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <div className={'icon'} id={'hack'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconCode/>
                         <div className={'labelText'}>Hackathon</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <div className={'icon'} id={'hunt'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconLocationPinAlt/>
                         <div className={'labelText'}>Scavenger <br/> Hunt</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                     <div className={'icon'} id={'tank'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconCashStack/>
                         <div className={'labelText'}>Maarif <br/> Tank</div>
+                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
+                            this
+                        </div>
                     </div>
                 </div>
                 <h1 className="text2">CATEGORIES</h1>
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -21,6 +21,7 @@ const Details = () => {
                     <h3>| Hassan Shahid: 0321-8890981 |</h3>
                     <h3>| Hammad Hussain: 0320-7819831 |</h3>
                     <h3>| Umar Raza: 0323-4468228 |</h3>
+                    <h3>| Syed Faakhir Hussain: 0309-4453826 |</h3>
                 </div>
             </div>
             <h3 className={'location'}>Pak-Turk Maarif International Schools and Colleges Lahore Boys Campus, Raiwind
