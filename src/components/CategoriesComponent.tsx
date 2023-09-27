fe9815bde326c1d83aacba49ae7ccf8e8dd00162
MatrioxPlayZ <shaharyar321321@gmail.com>
2023-09-27 15:06:07 +0500

    added learn more button

--- src/components/CategoriesComponent.tsx
+++ src/components/CategoriesComponent.tsx
@@ -16,13 +16,13 @@ const CategoriesComponent = () => {
         gsap.to(e.currentTarget, {duration: 0.2, scale: 1.1, ease: 'ease-out', boxShadow: '0px 0px 15px 3px rgba(73, 158, 155, 0.5)'})
         gsap.to(e.currentTarget.children[0], {duration: 0.2, opacity: 0, ease: 'ease-out'})
         gsap.to(e.currentTarget.children[1], {duration: 0.2, opacity: 0, ease: 'ease-out'})
-        gsap.to(e.currentTarget.children[2], {duration: 0.2, opacity: 1, ease: 'ease-out'})
+        gsap.to(e.currentTarget.children[2], {duration: 0.2, opacity: 1, ease: 'ease-out', delay: 0.1})
 
     }
     const handleLeave = (e: any) => {
         gsap.to(e.currentTarget, {duration: 0.2, scale: 1, ease: 'ease-out', boxShadow: '0px 0px 0px 0px rgba(0,0,0,0.75)'})
-        gsap.to(e.currentTarget.children, {duration: 0.2, opacity: 1, ease: 'ease-out', y: 0})
-        gsap.to(e.currentTarget.children[2], {duration: 0.2, opacity: 0, ease: 'ease-out'})
+        gsap.to(e.currentTarget.children, {duration: 0.2, opacity: 1, ease: 'ease-out', y: 0, delay: 0.1})
+        gsap.to(e.currentTarget.children[2], {duration: 0.2, opacity: 0, ease: 'ease-out', delay: 0.1})
 
     }
     return (
@@ -33,59 +33,43 @@ const CategoriesComponent = () => {
                     <div className={'icon'} id={'robotics'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconRobot/>
                         <div className={'labelText'}>Robotics</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                        <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <div className={'icon'} id={'math'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconBxMath/>
                         <div className={'labelText'}>Math Olympiad</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                       <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <div className={'icon'} id={'engineer'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <Icon150Cogs/>
                         <div className={'labelText'}>Engineering <br/>Encounters</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                      <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <div className={'icon'} id={'syrup'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconMasksTheater/>
                         <div className={'labelText'}>Syrup</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                        <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <br/>
                     <div className={'icon'} id={'cerca'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconHeadLightbulb/>
                         <div className={'labelText'}>Cerca-Trova</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                       <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <div className={'icon'} id={'hack'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconCode/>
                         <div className={'labelText'}>Hackathon</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                       <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <div className={'icon'} id={'hunt'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconLocationPinAlt/>
                         <div className={'labelText'}>Scavenger <br/> Hunt</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                      <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                     <div className={'icon'} id={'tank'} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
                         <IconCashStack/>
                         <div className={'labelText'}>Maarif <br/> Tank</div>
-                        <div className="shortDesc">Description Goes Here. This event is regardging this on the topic of
-                            this
-                        </div>
+                      <div className="shortDesc"><span className='learnMore'>Learn More</span></div>
                     </div>
                 </div>
                 <h1 className="text2">CATEGORIES</h1>
