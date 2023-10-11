fc3cb8a46ae72334678889a74f833f695a2ab917
MatrioxPlayZ <shaharyar321321@gmail.com>
2023-10-11 18:26:20 +0500

    map changes

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,8 +1,9 @@
 const MapComponent = () => {
     return (
         <>
+        <div className="iframeLink">
           <iframe width="100%" height="450" style={{border: 0}} loading="lazy" allowFullScreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJFfQ9gM4BGTkRELTdV0Nd_jE&key=AIzaSyAzl5AqguVVFrcUa66p-tOfMbjE_gWTsCo"></iframe>
-
+          </div>
         </>
     )
 }
